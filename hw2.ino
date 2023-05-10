17db4891ca25a1a215ab33b5ae32c3b59b18daaf
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-05-10 20:40:21 +0300

    fix: 🐛 fixed some problems

--- hw2.ino
+++ hw2.ino
@@ -1,19 +1,26 @@
 #include <LiquidCrystal_I2C.h>
 #include <MedianFilter.h>
+#include <Arduino_FreeRTOS.h>
 #include <EasyBuzzer.h>
+#include <semphr.h>
 
-#define BUZZER_PIN 7
+#define BUZZER_PIN 3
 #define TRIGGER_PIN A3
 #define ECHO_PIN A2
 
-// windows size: 5, seed 0
-MedianFilter filterObject(10, 0);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+void UltrasonicTask(void *pvParameters);
+void BuzzerTask(void *pvParameters);
 
+SemaphoreHandle_t mutex;
+
+// windows size: 6, seed 0
+MedianFilter filterObject(6, 0);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int duration;
 int led_period;
 
+int isBeeping = 0;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -29,46 +36,62 @@ void setup() {
   lcd.init();  //initialize the lcd
   lcd.setBacklight(HIGH);
 
-  lcd.clear();
-
-  lcd.setBacklight(HIGH);
-
   lcd.print("Distance: ");
+
+  mutex = xSemaphoreCreateMutex();
+
+  xTaskCreate(UltrasonicTask, "ultrsonic", 128, NULL, 1, NULL);
+
+
+  vTaskStartScheduler();
 }
 
 void loop() {
+}
 
-  unsigned int raw_distance = read_distance();  // overall delay between reads should be at least 60 ms
+void UltrasonicTask(void *pvParameters) {
 
-  /* should always be called for EasyBuzzer to work. */
-  EasyBuzzer.update();
+  while (1) {
 
-  unsigned int filtered_distance = filterObject.in(raw_distance);
+    if (xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE) {
+      /* obtained the semaphore and can now access the
+            shared resource. */
 
-  lcd.setCursor(0, 10);
+      unsigned int raw_distance = read_distance();  // overall delay between reads should be at least 60 ms
 
-  lcd.print(filtered_distance);
-  lcd.print("    ");
+      unsigned int filtered_distance = filterObject.in(raw_distance);
 
-  if (filtered_distance > 30) {
-    // noTone(BUZZER_PIN);
+      lcd.setCursor(0, 10);
 
-    EasyBuzzer.stopBeep();
+      lcd.print(filtered_distance);
+      lcd.print("    ");
 
-  } else {
+      if (filtered_distance > 30) {
+       
+        noTone(BUZZER_PIN);
 
-    EasyBuzzer.beep(
-      // filtered_distance  // Frequency in Hertz(HZ).
-      2
-    );
 
-    // tone(BUZZER_PIN, 10000/filtered_distance );
+      } else {
+
+        if (filtered_distance > 20) {
+          tone(BUZZER_PIN, 100);
+        } else if (filtered_distance > 10) {
+          tone(BUZZER_PIN, 3000);
+        } else {
+          tone(BUZZER_PIN, 10000);
+        }
+      }
+
+      xSemaphoreGive(mutex);
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
-
-  delay(600);
 }
 
 int read_distance() {
+
+  taskENTER_CRITICAL();
 
   unsigned int cm;
 
@@ -82,42 +105,9 @@ int read_distance() {
 
   cm = duration / 2 / 29.1 + 1;
 
+  taskEXIT_CRITICAL();
+
   return cm;
 }
 
 
-
-// function that reads 10 readings and removes max and min readings then returns the average
-// int read_distance_average()
-// {
-//   int readings[10];
-//   int i;
-//   int j;
-//   int temp;
-//   int average;
-//   int total;
-
-//   for (i = 0; i < 10; i++) {
-//     readings[i] = read_distance();
-//   }
-
-//   for (i = 0; i < 9; i++) {
-//     for (j = i + 1; j < 10; j++) {
-//       if (readings[i] > readings[j]) {
-//         temp = readings[i];
-//         readings[i] = readings[j];
-//         readings[j] = temp;
-//       }
-//     }
-//   }
-
-//   total = 0;
-
-//   for (i = 3; i < 7; i++) {
-//     total += readings[i];
-//   }
-
-//   average = total / 4;
-
-//   return average;
-// }
